9b3c155e0930a21bccc66414f5bde7844c3f7bb9
Crmiv <liujunning.magic@gmail.com>
2014-05-28 16:58:25 +0800

    htmlParser test

---
+++ test/htmlxhtmlparser/htmlParser.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from HTMLParser import HTMLParser
+#handle entity like &amp
+from htmlentitydefs import entitydefs
+import sys
+
+class TitleParser(HTMLParser):
+	def __init__(self):
+		self.title = ''
+		self.readingtitle = 0
+		HTMLParser.__init__(self)
+	
+	def handle_starttag(self, tag, attrs):
+		if tag == 'title':
+			self.readingtitle = 1 
+	
+	def handle_data(self, data):
+		if self.readingtitle:
+			self.title = data
+	
+	def handle_endtag(self, tag):
+		if tag == 'title':
+			self.readingtitle = 0
+
+	def gettitle(self):
+		return self.title
+	
+	def handle_entityref(self, name):
+		if entitydefs.has_key(name):
+			self.handle_data(entitydefs[name])
+		else:
+			self.handle_data('&' + name + ';')
+	
+	def handle_charref(self, name):
+		try:
+			charnum = int(name)
+		except ValueError:
+			return
+
+		if charnum < 1 or charnum > 255:
+			return
+
+		self.handle_data(chr(charnum))
+	
+
+fd = open(sys.argv[1])
+tp = TitleParser()
+tp.feed(fd.read())
+print tp.gettitle()
+
